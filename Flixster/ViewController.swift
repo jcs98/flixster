71127382dbf3a050107e406319c2b50c132a4fcf
Jainam Chirag Shah <jcshah98@gmail.com>
2022-02-22 17:37:40 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Jainam Chirag Shah on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
